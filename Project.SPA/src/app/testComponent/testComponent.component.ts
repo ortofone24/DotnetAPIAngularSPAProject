a8424b3d4ebfa8f98e47accb8fdead49e32677d0
MarcinGała <marcingala81@gimail.com>
2020-02-04 23:38:41 +0100

    Dyrektywy NG-Model

---
+++ Project.SPA/src/app/testComponent/testComponent.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-testComponent',
+  templateUrl: './testComponent.component.html',
+  styleUrls: ['./testComponent.component.css'],
+
+
+})
+export class TestComponentComponent implements OnInit {
+  name: string = '';
+
+  ngOnInit(): void { }
+
+
+
+  setValue() { this.name = 'Nancy'; }
+
+}
