7dc0a43dae7f69d33b1e9c0f8aa703f88068aff3
MarcinGała <marcingala81@gimail.com>
2020-02-07 13:05:56 +0100

    Dodanie metod logowania w nav component

--- Project.SPA/src/app/nav/nav.component.ts
+++ Project.SPA/src/app/nav/nav.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../_services/auth.service';
 
 @Component({
   selector: 'app-nav',
@@ -9,14 +10,28 @@ export class NavComponent implements OnInit {
 
   model: any = {};
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
 
   }
 
   login() {
-    console.log(this.model);
+    this.authService.login(this.model).subscribe(next => {
+      console.log('Zalogowałeś się do aplikacji');
+    }, error => {
+      console.log('Wystąpił błąd logowania');
+    });
+  }
+
+  loggedIn() {
+    const token = localStorage.getItem('token');
+    return !!token; // jeśli coś się znajduje w tokenia to return true jeśli będzie pusat to false
+  }
+
+  logout() {
+    localStorage.removeItem('token');
+    console.log('zostałeś wylogowany');
   }
 
 }
