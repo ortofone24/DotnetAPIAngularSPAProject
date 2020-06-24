cd134a5234b968baa9b2adf32c9ac4fa3e0e2955
MarcinGała <marcingala81@gimail.com>
2020-06-24 15:21:30 +0200

    Usuwanie zdjec SPA

--- Project.SPA/src/app/_services/user.service.ts
+++ Project.SPA/src/app/_services/user.service.ts
@@ -37,4 +37,8 @@ setMainPhoto(userId: number, id: number) {
   return this.http.post(this.baseUrl + 'users/' + userId + '/photos/' + id + '/setMain', {});
 }
 
+deletePhoto(userId: number, id: number) {
+  return this.http.delete(this.baseUrl + 'users/' + userId + '/photos/' + id);
+}
+
 }
