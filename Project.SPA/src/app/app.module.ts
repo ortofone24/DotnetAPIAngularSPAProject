0cebbd690552893b4124bf7f4ef1930e92bbd57a
MarcinGała <marcingala81@gimail.com>
2020-06-25 09:33:32 +0200

    Przejscie na formularz ReactiveForm

--- Project.SPA/src/app/app.module.ts
+++ Project.SPA/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule, HammerGestureConfig, HAMMER_GESTURE_CONFIG } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { JwtModule } from '@auth0/angular-jwt';
 import { RouterModule } from '@angular/router';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
@@ -62,6 +62,7 @@ export function tokenGetter() {
       BrowserModule,
       HttpClientModule,
       FormsModule,
+      ReactiveFormsModule,
       NgxGalleryModule,
       FileUploadModule,
       JwtModule.forRoot({
--- Project.SPA/src/app/register/register.component.ts
+++ Project.SPA/src/app/register/register.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
 import { AlertifyService } from '../_services/alertify.service';
+import { FormGroup, FormControl } from '@angular/forms';
 
 
 @Component({
@@ -11,21 +12,27 @@ import { AlertifyService } from '../_services/alertify.service';
 export class RegisterComponent implements OnInit {
 
 // @Input() valuesFromHome: any; // przyjecie wartości z komponentu home przez inputa
-@Output() cancelRegister = new EventEmitter(); // wysłanie wartości z komponentu dziecka do rodzica
-
+  @Output() cancelRegister = new EventEmitter(); // wysłanie wartości z komponentu dziecka do rodzica
   model: any = {};
+  registerForm: FormGroup;
 
   constructor(private authService: AuthService, private alertify: AlertifyService ) { }
 
   ngOnInit() {
+    this.registerForm = new FormGroup({
+      username: new FormControl(),
+      password: new FormControl(),
+      confirmPassword: new FormControl()
+    });
   }
 
   register() {
-    this.authService.register(this.model).subscribe(() => {
-      this.alertify.success('rejestracja udana');
-    }, error => {
-      this.alertify.error(error);
-    });
+    // this.authService.register(this.model).subscribe(() => {
+    //   this.alertify.success('rejestracja udana');
+    // }, error => {
+    //   this.alertify.error(error);
+    // });
+    console.log(this.registerForm.value)
   }
 
   cancel() {
