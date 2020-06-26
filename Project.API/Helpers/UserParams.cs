5771083a79a78819a056a16aa4530ec842c2665b
MarcinGała <marcingala81@gimail.com>
2020-06-26 11:59:26 +0200

    Ustawienia wstepna dla Paginacji API

---
+++ Project.API/Helpers/UserParams.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Project.API.Helpers
+{
+    public class UserParams
+    {
+        public const int MaxPageSize = 48;
+
+        public int PageNumber { get; set; } = 1;
+
+        private int pagesize = 24;
+
+        public int PageSize
+        {
+            get { return pagesize; }
+            set { pagesize = (value > MaxPageSize) ? MaxPageSize : value; }
+        }
+
+    }
+}
